09d1f95a9beb792004681fee42ef4e7994c79124
Antti Rintala <antti@rintala.it>
2017-08-22 21:01:43 +0300

    PoC with johnny-five

--- index.js
+++ index.js
@@ -2,5 +2,20 @@ const Controller = require('./lib/index.js');
 const johnnyFiveAdapter = require('./lib/adapters/johnny-five.js');
 const SuperCarController = new Controller();
 
-SuperCarController.setAdapter(johnnyFiveAdapter);
-SuperCarController.forward(100);
+SuperCarController.setAdapter(johnnyFiveAdapter, { repl: false });
+SuperCarController.adapterReady().then(() => {
+    SuperCarController.setAdapterConfiguration({
+        forward: {
+            pin: 10,
+            type: 'Relay'
+        }
+    });
+    SuperCarController.forward(100);
+    setTimeout(() => {
+        SuperCarController.forward(0);
+        process.exit(0);
+    }, 10000);
+}, () => {
+    console.log('Adapter bailed');
+    process.exit(1);
+});
